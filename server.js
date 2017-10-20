175af76da92d091c7253c78cc0d6b0b0a31b8d9b
OlySiStER <noopan.suphachok@gmail.com>
2017-10-20 22:31:50 +0700

    Loop + api
    Loop for show movie and information, api all

--- server.js
+++ server.js
@@ -13,10 +13,17 @@ app.use(expressSession({ secret: 'max', saveUninitialized: false, resave: false 
 
 app.get('/', (req, res) => {
     req.session.homepage = true;
-    res.render('index.hbs', {
-        ss_homepage: req.session.homepage,
-        login: req.session.alertLogin,
-        uname: req.session.uname
+    request({
+        url: 'http://localhost:3000/movie',
+        json: true
+    }, (error, response, body) => {
+        res.render('index.hbs', {
+            datas: body,
+            ss_homepage: req.session.homepage,
+            login: req.session.alertLogin,
+            uname: req.session.uname
+        });
+
     });
     req.session.homepage = false;
 });
@@ -93,16 +100,30 @@ app.get('/cartoon', (req, res) => {
     });
 });
 
-app.get('/bladerunner', (req, res) => {
-    res.render('bladerunner.hbs', {
-        login: req.session.alertLogin,
-        uname: req.session.uname
+// app.get('/bladerunner', (req, res) => {
+//     res.render('bladerunner.hbs', {
+//         login: req.session.alertLogin,
+//         uname: req.session.uname
+//     });
+// });
+
+app.get('/seemovie/:id', (req, res) => {
+    request({
+        url: 'http://localhost:3000/movie/'+req.params.id,
+        json: true
+    }, (error, response, body) => {
+        res.render('seemovie.hbs',{
+            informationmovie: body
+        });
+        // res.render('showinformationmovie.hbs', {
+        //     datas: body
+        // });
     });
 });
 
 //--------------------------- Admin -----------------------------------------------------------------
 app.get('/addmovie', (req, res) => {
-    console.log(req.session.addmoviecomplete);
+    // console.log(req.session.addmoviecomplete);
     res.render('addmovie.hbs', {
         addmoviecomplete: req.session.addmoviecomplete
     });
@@ -110,6 +131,13 @@ app.get('/addmovie', (req, res) => {
 });
 
 app.post('/addmovietodb', (req, res) => {
+    var d = new Date();
+    var m = d.getMinutes();
+    if(m<10) m = "0"+m;
+    var s = d.getSeconds();
+    if(s<10) s = "0"+s;
+    var dateTime = d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear() + " " + d.getHours() + ":" + m + ":" + s;
+    // console.log(dateTime);
     request.post(
         'http://localhost:3000/movie',
         { json: { 
@@ -118,19 +146,104 @@ app.post('/addmovietodb', (req, res) => {
             img: req.body.image,
             year: req.body.year,
             type: req.body.type,
-            createAt: Date()
+            trailer: req.body.trailer,
+            movieLink: req.body.movieLink,
+            createMovieDate: dateTime,
+            lastEdit: dateTime
         } },
         function (error, response, body) {
-            console.log(response.statusCode);
+            // console.log(response.statusCode);
             if (response.statusCode == "201") {
                 req.session.addmoviecomplete = true;
                 res.redirect('/addmovie');
             }
         }
     );
-
     // res.redirect('/addmovie');
-    
+});
+
+app.get('/getinformation/:id', (req, res) => {
+    request({
+        url: 'http://localhost:3000/movie/'+req.params.id,
+        json: true
+    }, (error, response, body) => {
+        res.render('showinformationmovie.hbs',{
+            informationmovie: body
+        });
+        // res.render('showinformationmovie.hbs', {
+        //     datas: body
+        // });
+    });
+});
+
+app.get('/editmovieform/:id', (req, res) => {
+    req.session.thaimovie = false;
+    req.session.othermovie = false;
+    req.session.cartoon = false;
+    request({
+        url: 'http://localhost:3000/movie/'+req.params.id,
+        json: true
+    }, (error, response, body) => {
+        // console.log(body.type);
+        if (body.type == "thaimovie"){
+            req.session.thaimovie = true
+        }else if (body.type == "othermovie"){
+            req.session.othermovie = true
+        }else if (body.type == "cartoon"){
+            req.session.cartoon = true
+        }
+        res.render('editmovieform.hbs',{
+            informationmovie: body,
+            ss_type_thaimovie: req.session.thaimovie,
+            ss_type_othermovie: req.session.othermovie,
+            ss_type_cartoon: req.session.cartoon
+        });
+    });
+});
+
+app.post('/editmovie/:id', (req, res) => {
+    // console.log(req.body.createMovieDate);
+    var d = new Date();
+    var m = d.getMinutes();
+    if(m<10) m = "0"+m;
+    var s = d.getSeconds();
+    if(s<10) s = "0"+s;
+    var dateTime = d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear() + " " + d.getHours() + ":" + m + ":" + s;
+    // console.log(dateTime);
+    request.put(
+        'http://localhost:3000/movie/'+req.params.id,
+        { json: { 
+            
+            title: req.body.title,
+            img: req.body.image,
+            year: req.body.year,
+            type: req.body.type,
+            trailer: req.body.trailer,
+            movieLink: req.body.movieLink,
+            createMovieDate: req.body.createMovieDate,
+            lastEdit: dateTime,
+            id: req.body.id
+        } },
+        function (error, response, body) {
+            // console.log(response.statusCode);
+            if (response.statusCode == "200") {
+                // req.session.addmoviecomplete = true;
+                res.redirect('/showlistmovie');
+            }
+        }
+    );
+});
+
+app.get('/delete/:id', (req, res) => {
+    // console.log("ok");
+    request.delete(
+        'http://localhost:3000/movie/'+req.params.id,
+        
+        function (error, response, body) {
+            // console.log(response.statusCode);
+            res.redirect('/showlistmovie');
+        }
+    );
 });
 
 app.get('/showlistmovie', (req, res) => {
@@ -145,6 +258,15 @@ app.get('/showlistmovie', (req, res) => {
     });
 });
 
+app.get('/getDateTime', (req, res) => {
+    var d = new Date();
+    var m = d.getMinutes();
+    if(m<10) m = "0"+m;
+    var s = d.getSeconds();
+    if(s<10) s = "0"+s;
+    var dateTime = d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear() + " " + d.getHours() + ":" + m + ":" + s;
+    console.log(dateTime);
+});
 
 app.listen(3001, () => {
     console.log('Server is up on port 3001');
