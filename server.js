ba8c0546ba955d1fa6db8177d7da30c5c705cdca
OlySiStER <noopan.suphachok@gmail.com>
2017-10-24 14:44:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const request = require('request');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 var bodyParser = require('body-parser');
 var app = express();
 var moment = require('moment');
